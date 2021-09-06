01f83a10356ea68168306ceb50d64033dace179c
José Andrés Lucatero Tenorio <benjamin.wwe@hotmail.es>
2021-09-06 14:36:50 -0500

    Add files via upload

---
+++ introduccion.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+void main() {
+	var dato = 3.14;
+  const datos_2 = 0.16; //valor conocido al momento de compilar
+  final fecha = new DateTime.now(); //valor no conocido al momento de compilar
+  int x = 20;
+  double y = 3.14159;
+
+  /*
+  int
+  float
+  String
+  bool
+  double
+
+  + - * / %
+  */
+
+  //
+
+  print(dato);
+  print(sin(45 * pi / 100));
+  print(sqrt(2));
+  print(3.14159.round());
+}
